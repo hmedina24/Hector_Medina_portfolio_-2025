555cb3be72df5f9f281838ec5eb9f4711274c8ae
hmedina24 <hectormedina@Hectors-MacBook-Air.local>
2025-06-11 17:35:02 -0400

    message

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -2,7 +2,6 @@ import React, {useEffect, useState} from "react";
 import Header from "../components/header/Header";
 import Greeting from "./greeting/Greeting";
 import Skills from "./skills/Skills";
-import StackProgress from "./skillProgress/skillProgress";
 import WorkExperience from "./workExperience/WorkExperience";
 import Projects from "./projects/Projects";
 import StartupProject from "./StartupProjects/StartupProject";
@@ -55,7 +54,6 @@ const Main = () => {
             <Skills />
             <WorkExperience />
             <Education />
-            <StackProgress />
             <Projects />
             <StartupProject />
             <Achievement />
