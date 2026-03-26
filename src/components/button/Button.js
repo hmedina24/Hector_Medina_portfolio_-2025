8f24ff048b51a64508d6d936f6fb1b64ec5c8dc9
Hector Medina <hectormedina24@icloud.com>
2026-03-25 22:06:45 -0400

    Induced Prettify action on button.js file.

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -4,20 +4,17 @@ import "./Button.scss";
 export default function Button({text, className, href, newTab}) {
   if (href) {
     return (
-    <div className={className}>
-      <a className="main-button" href={href} target={newTab && "_blank"}>
-        {text}
-      </a>
-    </div>
+      <div className={className}>
+        <a className="main-button" href={href} target={newTab && "_blank"}>
+          {text}
+        </a>
+      </div>
     );
   }
 
   return (
     <div className={className}>
-      <button className="main-button">
-        {text}
-      </button>
+      <button className="main-button">{text}</button>
     </div>
   );
-  
 }
