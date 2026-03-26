972a32ff536a85fdd99326d2f853ba516265111d
Hector Medina <hectormedina24@icloud.com>
2026-03-25 22:03:04 -0400

    Fix: implemented Button render conditionally:
    - With href prop: Renders as an <a> tag (for the "Contact me" button)
    - Without href prop: Renders as a <button> element (for the "Download my resume" button inside the <a> tag)
    
    eliminates the invalid nested anchor tags while maintaining your original styling and functionality.
    
    Fix # (https://github.com/hmedina24/Hector_Medina_portfolio_-2025/issues/41)

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -2,11 +2,22 @@ import React from "react";
 import "./Button.scss";
 
 export default function Button({text, className, href, newTab}) {
-  return (
+  if (href) {
+    return (
     <div className={className}>
       <a className="main-button" href={href} target={newTab && "_blank"}>
         {text}
       </a>
     </div>
+    );
+  }
+
+  return (
+    <div className={className}>
+      <button className="main-button">
+        {text}
+      </button>
+    </div>
   );
+  
 }
